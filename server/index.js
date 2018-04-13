d678e847cfae338accb75ea58001da4b8cf3dbdb
pm-simrendrasingh-shahoo <simrendrasingh.shahoo@pubmatic.com>
2018-04-13 17:18:15 +0530

    fixed Issue#1

---
+++ server/index.js
@@ -0,0 +1,21 @@
+
+(function() {
+    var server = require('ws').Server;
+
+    var s = new server({
+        port: 5001
+    });
+
+    s.on('connection', function(ws) {
+        ws.on('message', function(message) {
+            s.clients.forEach(function(client) {
+                if(client !== ws) {
+                    client.send(message);
+                }
+
+            });
+            
+        });
+        
+    });
+})();
